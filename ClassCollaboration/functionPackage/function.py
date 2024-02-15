10c609a4a16344ef08b1cb8953870c171034df05
brian <brian@Bri.wifi.uc.edu>
2024-02-15 08:50:10 -0500

    Function Commit

--- ClassCollaboration/functionPackage/function.py
+++ ClassCollaboration/functionPackage/function.py
@@ -1,3 +1,13 @@
-# .py
+# functions.py
+# Name: Brianna Jarrell
+# email: jarrelbc@mail.uc.edu
+# Assignment Number: 
+# Due Date:
+# Course/Section: IS 4010 - 002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: Learning how to use CSV files
+# Citations: 
+# Anything else that's relevant:
+
 def jarrelbc():
     print("Brianna Jarrell")
